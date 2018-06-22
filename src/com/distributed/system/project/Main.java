80b46ef3cf87df81c2ad4a0818250d76d5330c5a
Hussein Awala <houssein.awala.96@gmail.com>
2018-06-22 13:01:02 +0300

    Adding SingRequest and some fixes in security class

--- src/com/distributed/system/project/Main.java
+++ src/com/distributed/system/project/Main.java
@@ -1,8 +1,23 @@
 package com.distributed.system.project;
 
+
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        KeyPair keyPair=security.buildKeyPair();
+        byte[] a=security.encrypt(keyPair.getPublic(),"Hussein");
+        for (int i=0;i<a.length;i++){
+            System.out.print((char)a[i]);
+        }
+        System.out.println();
+        System.out.println(security.decrypt(keyPair.getPrivate(),a));
+        byte[] b=security.decrypt(keyPair.getPrivate(),a);
+        for (int i=0;i<b.length;i++){
+            System.out.print((char)b[i]);
+        }
     }
 }
